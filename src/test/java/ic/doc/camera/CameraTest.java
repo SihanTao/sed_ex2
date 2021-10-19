cbf4c99e30ce01abb8ad9f80a59880aa7acb5964
st2220 <sihan.tao@imperial.ac.uk>
2021-10-19 16:02:29 +0100

    Implement and pass the test for switchingTheCameraOnPowersUpTheSensor.

--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -1,18 +1,30 @@
 package ic.doc.camera;
 
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static org.jmock.internal.Cardinality.exactly;
+
 public class CameraTest {
 
-  @Rule
-  public JUnitRuleMockery context = new JUnitRuleMockery();
+  @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+
+  Sensor sensor = context.mock(Sensor.class);
 
   @Test
   public void switchingTheCameraOnPowersUpTheSensor() {
 
-    // write your test here
-  }
+    Camera camera = new Camera(sensor);
 
+    context.checking(
+        new Expectations() {
+          {
+            exactly(1).of(sensor).powerUp();
+          }
+        });
+
+    camera.powerOn();
+  }
 }
